025c74f71df8b61760de9e1ce75c2151fbc27947
Matheus <matheussmoura@outlook.com>
2024-03-01 19:37:14 -0300

    iniciado projeto

---
+++ src/main/java/com/example/atacadista/AtacadistaApplication.java
@@ -0,0 +1,13 @@
+package com.example.atacadista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtacadistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtacadistaApplication.class, args);
+	}
+
+}
