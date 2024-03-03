9105bcc76ef87b45e1ab1dc7d93a07d1a80600cb
Matheus <matheussmoura@outlook.com>
2024-03-03 20:11:51 -0300

    chore: pesquisa de pedido por codigo e por cliente

--- src/main/java/com/example/atacadista/controller/PedidoController.java
+++ src/main/java/com/example/atacadista/controller/PedidoController.java
@@ -7,6 +7,7 @@ import com.example.atacadista.service.PedidoService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -15,14 +16,27 @@ import java.util.List;
 public class PedidoController {
 
     private final PedidoService pedidoService;
+    private final PedidoRepository pedidoRepository;
 
-    public PedidoController(PedidoService pedidoService) {
+    public PedidoController(PedidoService pedidoService, PedidoRepository pedidoRepository) {
         this.pedidoService = pedidoService;
+        this.pedidoRepository = pedidoRepository;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Pedido novoPedido(@Valid @RequestBody PedidoCadastroDTO dto) {
         return pedidoService.cadastrarPedido(dto);
+    }
+
+    @GetMapping("{codigo}")
+    public Pedido pedidoPorCodigo(@PathVariable Long codigo) {
+        return pedidoRepository.findById(codigo)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Item inexistente"));
+    }
+
+    @GetMapping
+    public List<Pedido> pedidoPorCliente(@RequestParam String cpf) {
+        return pedidoRepository.findByCliente(cpf);
     }
 }
--- src/main/java/com/example/atacadista/repository/PedidoRepository.java
+++ src/main/java/com/example/atacadista/repository/PedidoRepository.java
@@ -2,5 +2,18 @@ package com.example.atacadista.repository;
 
 import com.example.atacadista.domain.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {}
+import java.util.List;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    @Query(value = """
+            SELECT p
+            FROM Pedido p
+            INNER JOIN Cliente c
+                ON c.codigo = p.cliente.codigo
+            WHERE c.cpf = :cpf
+            """)
+    List<Pedido> findByCliente(String cpf);
+}
