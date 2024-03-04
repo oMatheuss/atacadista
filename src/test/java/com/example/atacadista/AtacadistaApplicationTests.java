aeaed42b7b6b40aee13cb4920a126a0a8ca74614
Matheus <matheussmoura@outlook.com>
2024-03-03 21:40:54 -0300

    chore: adicionado mockito & adicionado teste basico

--- src/test/java/com/example/atacadista/AtacadistaApplicationTests.java
+++ src/test/java/com/example/atacadista/AtacadistaApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.atacadista;
 
+import com.example.atacadista.domain.Cliente;
+import com.example.atacadista.repository.ClienteRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AtacadistaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ClienteRepository clienteRepository;
+
+    @Test
+    void testSaveCliente() throws Exception {
+        // cpf está errado, e a requisição deve retornar status 400
+        mockMvc.perform(post("/api/cliente")
+                        .content("{ \"cpf\": \"abc\", \"nome\": \"Matheus\", \"uf\": \"MG\" }")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        // tudo ok, deve retornar 201
+        mockMvc.perform(post("/api/cliente")
+                        .content("{ \"cpf\": \"12345678912\", \"nome\": \"Matheus\", \"uf\": \"MG\" }")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        // a função save do jpa deve ter sido chamada uma vez
+        verify(clienteRepository, times(1)).save(any(Cliente.class));
+    }
 
 }
