8371888896c5ea960ae616311058537cfdb83ec3
Matheus <matheussmoura@outlook.com>
2024-03-06 19:11:02 -0300

    chore: validação para o percentualMaximoDesconto

--- src/main/java/com/example/atacadista/dto/ProdutoCadastroDTO.java
+++ src/main/java/com/example/atacadista/dto/ProdutoCadastroDTO.java
@@ -1,5 +1,7 @@
 package com.example.atacadista.dto;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -17,5 +19,7 @@ public record ProdutoCadastroDTO(
     @NotNull(message = "O campo preco é obrigatório")
     double preco,
 
+    @Digits(integer = 3, fraction = 2, message = "O campo percentualMaximoDesconto pode variar de 0 a 100 com no máximo duas casas decimais")
+    @Max(value = 100, message = "O campo percentualMaximoDesconto deve estar entre 0 e 100")
     float percentualMaximoDesconto
 ) {}
